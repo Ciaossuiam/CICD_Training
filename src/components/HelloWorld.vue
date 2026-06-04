083459d389dadf68124e4fca73bde5bc5a83d858
IAMFRONTEND\IamFr <mouth@retailingdata.com.tw>
2026-06-04 12:29:05 +0800

    TEST

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -82,7 +82,7 @@ const count = ref(0)
             <svg class="button-icon" role="presentation" aria-hidden="true">
               <use href="/icons.svg#bluesky-icon"></use>
             </svg>
-            Bluesky
+            Blueskys
           </a>
         </li>
       </ul>
